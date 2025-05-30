b18f8db188460b14e722eac0596acc89570787b1
pdefd77 <pdefd77@gmail.com>
2025-05-30 15:42:59 +0900

    [feat] 상점 구매 기능에 레이어마스크 추가
    플레이어(움직이는 것)과 충돌 예외 처리

--- Assets/SDH/Scripts/Managers/StageManager.cs
+++ Assets/SDH/Scripts/Managers/StageManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class StageManager // 씬 전환 관리 (전투-상점 등)
@@ -14,4 +15,18 @@ public class StageManager // 씬 전환 관리 (전투-상점 등)
         }
     }
     private float stageTime; // 스테이지 전환 시간
+
+    public IEnumerator StartStage() // 스테이지 시작
+    {
+        float leftTime = stageTime; // 버텨야 하는 시간
+
+        while (leftTime > 0)
+        {
+            leftTime -= Time.deltaTime;
+
+            yield return null;
+        }
+
+        // 스테이지 클리어
+    }
 }
--- Assets/SDH/Scripts/Shop/ShopControl.cs
+++ Assets/SDH/Scripts/Shop/ShopControl.cs
@@ -1,8 +1,15 @@
+using System.Collections;
 using UnityEngine;
 
-public class Shopper : MonoBehaviour // 아이템 구매하고 적용하는 상점 시스템
+public class ShopControl : MonoBehaviour // 아이템 구매하고 적용하는 상점 시스템
 {
     private Transform player;
+    private LayerMask shopLayerMask;
+
+    private void Awake()
+    {
+        shopLayerMask = ~LayerMask.GetMask("Character", "Flyer");
+    }
 
     private void Start()
     {
@@ -11,7 +18,7 @@ public class Shopper : MonoBehaviour // 아이템 구매하고 적용하는 상점 시스템
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.V))
         {
             BuyItem();
         }
@@ -29,9 +36,7 @@ public class Shopper : MonoBehaviour // 아이템 구매하고 적용하는 상점 시스템
 
     private void BuyItem() // 버튼을 누르면 아이템 구매
     {
-        Debug.Log("구매 시도");
-
-        Collider2D[] colliders = Physics2D.OverlapPointAll(player.transform.position); // 레이어 체크 필요 (플레이어 내 콜라이더와 겹침)
+        Collider2D[] colliders = Physics2D.OverlapPointAll(player.transform.position, shopLayerMask);
 
         if (colliders.Length == 0) return;
 
