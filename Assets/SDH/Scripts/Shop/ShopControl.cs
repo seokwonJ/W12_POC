05a9962fd8b2511920f21fa18235abe97c16a103
pdefd77 <pdefd77@gmail.com>
2025-05-30 16:35:29 +0900

    [style] 주석 추가

--- Assets/SDH/Scripts/Shop/ShopControl.cs
+++ Assets/SDH/Scripts/Shop/ShopControl.cs
@@ -8,7 +8,7 @@ public class ShopControl : MonoBehaviour // 아이템 구매하고 적용하는 상점 시스템
 
     private void Awake()
     {
-        shopLayerMask = ~LayerMask.GetMask("Character", "Flyer");
+        shopLayerMask = ~LayerMask.GetMask("Character", "Flyer"); // 영웅(Character) 비행체(Flyer) 제외. 상점 상품 전용 레이어가 생긴다면 그렇게 넣는 것도 가능
     }
 
     private void Start()
@@ -36,7 +36,7 @@ public class ShopControl : MonoBehaviour // 아이템 구매하고 적용하는 상점 시스템
 
     private void BuyItem() // 버튼을 누르면 아이템 구매
     {
-        Collider2D[] colliders = Physics2D.OverlapPointAll(player.transform.position, shopLayerMask);
+        Collider2D[] colliders = Physics2D.OverlapPointAll(player.transform.position, shopLayerMask); // 사실 상품 콜라이더가 겹칠 일은 없으니 OverlapPoint만 해도 충분한 거 아닐까
 
         if (colliders.Length == 0) return;
 
