838f435a1943d2c7d895a18630854a42b392d7a7
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-06-03 16:55:57 +0900

    [add] enemy 피격 이펙트

--- Assets/JSW/Scripts/Enemy/EnemyHP.cs
+++ Assets/JSW/Scripts/Enemy/EnemyHP.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 public class EnemyHP : MonoBehaviour
 {
     public int enemyHP;
-    public Material damagedMat;
+    private Renderer renderer;
     private WaitForSeconds flashDuration = new WaitForSeconds(0.1f);
+    private float dieDelay = 1f;
+    private WaitForSeconds dieEffectDuration;
+
+    private void Awake()
+    {
+        dieEffectDuration = new WaitForSeconds(dieDelay);
+        renderer = GetComponent<Renderer>();
+
+    }
 
     public void TakeDamage(int hp)
     {
         enemyHP -= hp;
-        //StartCoroutine(getDmagedEffect());
+        StartCoroutine(getDmagedEffect());
 
         if (enemyHP <= 0)
         {
@@ -22,12 +31,13 @@ public class EnemyHP : MonoBehaviour
     {
 
 
-        Destroy(gameObject);
+        Destroy(gameObject, dieDelay);
     }
-    //IEnumerator getDmagedEffect()
-    //{
-    //    //damagedMat.SetFloat("_FlashAmount", 1f);
-    //    //yield return flashDuration;
-    //    //damagedMat.SetFloat("_FlashAmount", 0f);
-    //}
+    IEnumerator getDmagedEffect()
+    {
+        Debug.Log("getDmagedEffect");
+        renderer.material.EnableKeyword("_ISFLASHED");
+        yield return flashDuration;
+        renderer.material.DisableKeyword("_ISFLASHED");
+    }
 }
