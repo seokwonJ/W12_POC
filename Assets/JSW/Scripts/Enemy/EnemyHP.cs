7fc7e73fd078b0a744b62e677e619476cb9cedbe
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-06-03 17:48:58 +0900

    [add] enemy 사망 이펙트 임시

--- Assets/JSW/Scripts/Enemy/EnemyHP.cs
+++ Assets/JSW/Scripts/Enemy/EnemyHP.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 public class EnemyHP : MonoBehaviour
 {
     public int enemyHP;
+    private bool isDead = false;
+
     private Renderer renderer;
+    private Collider collider;
     private WaitForSeconds flashDuration = new WaitForSeconds(0.1f);
     private float dieDelay = 1f;
-    private WaitForSeconds dieEffectDuration;
+
 
     private void Awake()
     {
-        dieEffectDuration = new WaitForSeconds(dieDelay);
+        //dieEffectDuration = new WaitForSeconds(dieDelay);
         renderer = GetComponent<Renderer>();
+        collider = GetComponent<Collider>();
 
     }
 
     public void TakeDamage(int hp)
     {
+        if (isDead) return;
+
         enemyHP -= hp;
-        StartCoroutine(getDmagedEffect());
+        StartCoroutine(CoDamagedEffect());
 
         if (enemyHP <= 0)
         {
@@ -29,15 +35,29 @@ public class EnemyHP : MonoBehaviour
 
     public void Die()
     {
-
-
-        Destroy(gameObject, dieDelay);
+        isDead = true;
+        StartCoroutine(CoDieEffect());
     }
-    IEnumerator getDmagedEffect()
+    IEnumerator CoDamagedEffect()
     {
-        Debug.Log("getDmagedEffect");
         renderer.material.EnableKeyword("_ISFLASHED");
         yield return flashDuration;
         renderer.material.DisableKeyword("_ISFLASHED");
+    }
+
+    IEnumerator CoDieEffect()
+    {
+        // _DieEffectValue가  dieEffectDuration 시간에 걸쳐 1에서 0으로 감소
+        yield return flashDuration;
+        float elapsedTime = 0f;
+        while (elapsedTime < dieDelay)
+        {
+            float dieEffectValue = 1f - (elapsedTime / dieDelay);
+            renderer.material.SetFloat("_DieEffectValue", dieEffectValue);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        Destroy(gameObject);
+
     }
 }
