592229062b9495a70acecdbedbd3478eda851ebd
SEOKWON <bbb10311031@gmail.com>
2025-06-04 20:37:36 +0900

    [merge] 오류 수정

--- Assets/SDH/Scripts/Stage/StageControlDH.cs
+++ Assets/SDH/Scripts/Stage/StageControlDH.cs
@@ -17,7 +17,9 @@ public class StageControlDH : MonoBehaviour // Àû ½ºÆù°ú ½ºÅ×ÀÌÁö Á¾·á¸¦ ÄÁÆ®·ÑÇ
 
     private void Start()
     {
+        Debug.Log(Managers.Stage.Stage);
         nowStage = Managers.Stage.GetNowStageTemplate();
+        Debug.Log(nowStage);
 
         StartCoroutine(CoSpawnEnemyRoutine(nowStage.enemyWave, nowStage.WaveCount, nowStage.WaveInterval, nowStage.stagePlayTime));
     }
