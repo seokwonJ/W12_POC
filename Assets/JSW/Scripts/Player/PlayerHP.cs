12f1a37abdb2ad6d3f7721bf284f60f3682b1a50
vcds1029 <vcds1029@gmail.com>
2025-06-05 23:31:43 +0900

    [feat] HP BAR 스크립트 생성
    [feat] HP BAR 스크립트 생성

--- Assets/JSW/Scripts/Player/PlayerHP.cs
+++ Assets/JSW/Scripts/Player/PlayerHP.cs
@@ -5,11 +5,31 @@ public class PlayerHP : MonoBehaviour
 {
     public Image playerHP_Image;
     private PlayerStatus _playerStatus;
+    [HideInInspector]
+    public GameObject hpBarObject;
+
+    // 추가: HPBar 프리팹과 캔버스 Transform을 Inspector에서 할당
+    public GameObject hpBarPrefab;
+    public Transform canvasTransform;
+    public Vector3 hpBarOffset;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     protected virtual void Start()
     {
         _playerStatus = GetComponent<PlayerStatus>();
+
+        // HPBar 프리팹을 직접 생성하고 연결
+        if (hpBarPrefab != null && canvasTransform != null)
+        {
+            hpBarObject = Instantiate(hpBarPrefab, canvasTransform);
+            HPBar hpBarScript = hpBarObject.GetComponent<HPBar>();
+            if (hpBarScript != null)
+            {
+                hpBarScript.target = this.transform;
+                hpBarScript.offset = hpBarOffset;
+            }
+            playerHP_Image = hpBarObject.GetComponent<Image>();
+        }
     }
     
     public virtual void TakeDamage(int damage)
@@ -21,6 +41,7 @@ public class PlayerHP : MonoBehaviour
         {
             GetComponent<TmpPlayerControl>().GatherCharacters();
             Managers.SceneFlow.GameOver();
+            if (hpBarObject != null) Destroy(hpBarObject);
         }
     }
 }
