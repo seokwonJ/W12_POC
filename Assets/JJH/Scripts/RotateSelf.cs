4dd5662fe29f24da65add9f76e5d80723cd27640
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-06-05 13:22:39 +0900

    [mod] 기본 projectile 스프라이트 변경

---
+++ Assets/JJH/Scripts/RotateSelf.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class RotateSelf : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 360f;
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+}
