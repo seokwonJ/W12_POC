fa2b5fa00e97998173c0f39340f47e05c5e1edb9
pdefd77 <pdefd77@gmail.com>
2025-06-05 19:21:30 +0900

    [update] 스테이지 정보 정렬 기능 추가

--- Assets/SDH/Scripts/Managers/StageManager.cs
+++ Assets/SDH/Scripts/Managers/StageManager.cs
@@ -58,6 +58,7 @@ public class StageManager // 씬 전환 관리 (전투-상점 등)
     public void Init()
     {
         stageTemplates = Resources.LoadAll<StageSO>("StageTemplates");
+        Array.Sort(stageTemplates, (a, b) => a.world == b.world ? a.stage.CompareTo(b.stage) : a.world.CompareTo(b.world)); // stageSO를 순서대로 정렬
     }
 
     public void StartGame() // 게임 시작. 현재는 스테이지 변수 초기화만 있으며 아무것도 안함 이거 수정하면서 위에 변수들 초기화 설정값 변경할 것 (필드를 시작할 때 값을 수정하므로 유의)
@@ -66,8 +67,13 @@ public class StageManager // 씬 전환 관리 (전투-상점 등)
         stage = 0;
     }
 
+    public void StartStage() // 현재 스테이지 시작
+    {
+
+    }
+
     public StageSO GetNowStageTemplate() // 현재 스테이지 템플릿 가져오기
     {
-        return Array.Find(stageTemplates, stageSO => stageSO.world == world && stageSO.stage == stage);
+        return Array.Find(stageTemplates, stageSO => stageSO.world == world && stageSO.stage == stage); // stageTemplates를 정렬해뒀기 때문에 나중에 이부분 수정 가능 @@@@@@@@@@@@@@@@@@@@@@@@@@
     }
 }
