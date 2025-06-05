b46d6c07eadd010efb6688b00c83b6c913fcf613
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-06-05 19:49:24 +0900

    [add] coin prefab 생성

---
+++ Assets/JJH/Scripts/Coin.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int value;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Managers.Status.Gold += value;
+            // to do 코인 획득 이펙트
+            Debug.Log($"Coin collected! Value: {value}, Total Gold: {Managers.Status.Gold}");
+            Destroy(gameObject);
+        }
+    }
+}
