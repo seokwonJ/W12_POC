ada8ea5377e7a8f71201c51422fd9b4b3dc9cbbf
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-06-09 13:48:41 +0900

    [add] 1,2 스테이지와 3스테이지에 다른 배경화면 사용

---
+++ Assets/JJH/Scripts/BackGround/BackGrounds.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BackGrounds : MonoBehaviour
+{
+    public GameObject[] backgrounds; // 배경 오브젝트 배열
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        foreach(GameObject bg in backgrounds)
+        {
+            bg.SetActive(false); // 모든 배경 비활성화
+        }
+
+        int stageNum = Managers.Stage.Stage;
+        if (stageNum >= 0 && stageNum < backgrounds.Length)
+        {
+            backgrounds[stageNum].SetActive(true); // 현재 스테이지에 해당하는 배경 활성화
+        }
+        else
+        {
+            Debug.LogWarning("Invalid stage number: " + stageNum);
+        }
+    }
+}
