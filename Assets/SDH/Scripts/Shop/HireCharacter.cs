2d030c36e40ef9f9e93abe40e4a11abc841115ae
pdefd77 <pdefd77@gmail.com>
2025-06-12 10:11:23 +0900

    [fix] 동료 고용이 다 찼을 때 창이 안사라지는 버그 수정

--- Assets/SDH/Scripts/Shop/HireCharacter.cs
+++ Assets/SDH/Scripts/Shop/HireCharacter.cs
@@ -10,7 +10,11 @@ public class HireCharacter : MonoBehaviour // 상점 시작 전 동료 한 명 고용
 
     private void Start()
     {
-        if (Managers.PlayerControl.Characters.Count >= 4) return; // 동료가 다 차면 고용하지 않음 (해고나 동료 최대치 변경이 있다면 이 부분 수정 필요)
+        if (Managers.PlayerControl.Characters.Count >= 4) // 동료가 다 차면 고용하지 않음 (해고나 동료 최대치 변경이 있다면 이 부분 수정 필요)
+        {
+            Destroy(hireCanvas);
+            return;
+        }
 
         Managers.PlayerControl.NowPlayer.SetActive(false); // 이거 임시코드 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
