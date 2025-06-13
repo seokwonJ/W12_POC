a81275ad13b0343a968f84702ba2f3431717bbc9
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-06-13 11:32:54 +0900

    [mod] 1-1 ~ 1-3 스테이지

--- Assets/JJH/Scripts/StageSO.cs
+++ Assets/JJH/Scripts/StageSO.cs
@@ -6,7 +6,6 @@ public class StageSO : ScriptableObject
     public int world; // 월드 번호, 1-2 일 경우 1
     public int stage; // 스테이지 번호, 1-2 일 경우 2
     public bool isBossStage; // 보스 스테이지 여부
-    public int enemyNumLimit; // 스테이지에서 더 이상 소환하지 않을 만큼의 적의 개수
 
     public EnemyWaveSO[] enemyWave; // 각 웨이브의 적 구성
     public int[] waveCount; // 각 웨이브를 몇 번씩 소환할지 개수
