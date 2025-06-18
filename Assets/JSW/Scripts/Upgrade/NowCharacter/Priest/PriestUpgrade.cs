230f9397d21b74e37508bf241bb115dcb27a0e38
SEOKWON <bbb10311031@gmail.com>
2025-06-18 10:50:49 +0900

    [feat] priest 작업 완료

--- Assets/JSW/Scripts/Upgrade/NowCharacter/Priest/PriestUpgrade.cs
+++ Assets/JSW/Scripts/Upgrade/NowCharacter/Priest/PriestUpgrade.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Searcher;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "PriestUpgrade", menuName = "Upgrades/PriestUpgrade")]
@@ -14,7 +15,12 @@ public class PriestUpgrade : CharacterUpgrade
         CriticalDamageUp,                           // 크리 피해 배수 증가
         AttackRangeUp,                              // 적 감지/공격 가능 거리 확대
         ManaRegenSpeedDownAttackPowerUp,            // 마나 회복 속도 감소 + 공격력 증가
-        ManaRegenSpeedUPAbilityPowerUp             // 마나 회복 속도 증가 + 스킬 대미지 증가
+        ManaRegenSpeedUPAbilityPowerUp,             // 마나 회복 속도 증가 + 스킬 대미지 증가
+
+        SkillDurationUp,                            // 스킬의 지속시간 증가
+        SkillCharacterAttackUp,                     // 스킬 시전 시 아군 전체 공격력 증가
+        SkillPlayerSpeedUp,                         // 스킬 시전 시 비행체의 이속을 % 증가시킴
+        AttackEnemyDefenseDown,                     // 사제의 공격이 방어력을 감소 시킵니다
     }
 
     public UpgradeType type;
@@ -76,8 +82,29 @@ public class PriestUpgrade : CharacterUpgrade
                 Debug.Log("Debug9 priest");
                 priest.upgradeNum = 9;
                 break;
-                //-------------- 특수 업그레이드 --------------
 
+            //-------------- 특수 업그레이드 --------------
+
+            case UpgradeType.SkillDurationUp:                                                          // 스킬의 지속시간 증가
+                priest.skillDuration += 2;
+                Debug.Log("Debug10 priest");
+                priest.upgradeNum = 10;
+                break;
+            case UpgradeType.SkillCharacterAttackUp:                                                   // 스킬 시전 시 아군 전체 공격력 증가
+                priest.isUpgradeSkillCharacterAttackUp = true;
+                Debug.Log("Debug11 priest");
+                priest.upgradeNum = 11;
+                break;
+            case UpgradeType.SkillPlayerSpeedUp:                                                       // 스킬 시전 시 비행체의 이속을 % 증가시킴
+                priest.isUpgradeSkillPlayerSpeedUp = true;
+                Debug.Log("Debug12 priest");
+                priest.upgradeNum = 12;
+                break;
+            case UpgradeType.AttackEnemyDefenseDown:                                                   // 사제의 공격이 방어력을 감소 시킵니다
+                priest.isUpgradeAttackEnemyDefenseDown = true;
+                Debug.Log("Debug13 priest");
+                priest.upgradeNum = 13;
+                break;
         }
 
         Debug.Log("업그레이드 성공");
