0118246eae2e5bb7611ffd8ffe0e532eb701a3db
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-06-18 14:05:25 +0900

    [mod] necromancer 공격 패턴 완성

--- Assets/JJH/Scripts/Enemy/Attacks/NecromancerAttack.cs
+++ Assets/JJH/Scripts/Enemy/Attacks/NecromancerAttack.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.LightTransport;
+
 [CreateAssetMenu(menuName = "Patterns/Attack/NecromancerAttack")]
 public class NecromancerAttack : ScriptableObject, IAttackPattern
 {
@@ -30,39 +31,23 @@ public class NecromancerAttack : ScriptableObject, IAttackPattern
     private float beforeMeidiumProjectileCoolDown = 1f; // 중간 크기 발사체 생성 전 애니메이션만 바뀌는 시간
     private WaitForSeconds beforeMediumProjectileWait;
 
-    [Header("큰 크기의 발사체 오른쪽에서 발사하는 패턴 관련")]
-    public GameObject largeProjectilePrefab; // 큰 크기 발사 웨이브
-    private int largeProjectileCount = 9; // 큰 크기 발사 횟수
-    private int largeProjectileNumPerSpawn = 5; // 한 번에 생성되는 큰 크기 발사체 수
-    private float largeProjectileSpeed = 16f; // 큰 크기 발사체 속도
-    private float largeProjectileCoolDown = 0.3f; // 큰 크기 발사체 생성 간격
-    private WaitForSeconds largeProjectileWait;
-    private float afterLargePjojectileCoolDown = 2.5f; // 큰 크기 발사체 생성 후 대기 시간
-    private WaitForSeconds afterLargeProjectileWait;
-
-    private Vector2 spawnMissleMin; 
-    private Vector2 spawnMissileMax;
-
     [Header("해골 발사체 발사하는 패턴 관련")]
     public EnemyWaveSO skullProjectileWave; // 해골 발사 웨이브
     private int skullProjectileCount = 2; // 해골 발사 횟수
+    private WaitForSeconds beforeSkullProjectileWait = new WaitForSeconds(1f);
     private WaitForSeconds afterSkullProjectileWait = new WaitForSeconds(8f);
 
+    [Header("폭발 패턴 관련")]
+    public GameObject explosionPrefab; // 폭발 프리팹
+    private int explosionCount = 6; // 큰 크기 발사 횟수
+    private WaitForSeconds explosionWait = new WaitForSeconds(1.5f);
 
 
-    [Header("대쉬 패턴 관련")]
 
-    private int dashActivateCount = 0; // 대쉬 패턴을 실행한 횟수
-    private float dashSpeed = 30f; // 대쉬 속도
-    private float beforeDashCoolDown = 1f; // 대쉬 전 애니메이션만 바뀌는 시간
-    private WaitForSeconds beforeDashCoolDownWait;
-    private float dashDuration = 2f; // 대쉬 지속 시간
-    private WaitForSeconds dashDurationWait;
 
     public void Init(Enemy enemy)
     {
         Debug.Log(Time.deltaTime);
-        dashActivateCount = 0;
         enemyHP = enemy.GetComponent<EnemyHP>();
         this.enemy = enemy;
         anim = enemy.GetComponent<Animator>();
@@ -74,13 +59,6 @@ public class NecromancerAttack : ScriptableObject, IAttackPattern
         mediumProjectileWait = new WaitForSeconds(mediumProjectileCoolDown);
         mediumOneProjectileWait = new WaitForSeconds(mediumOneProjectileCoolDown);
         beforeMediumProjectileWait = new WaitForSeconds(beforeMeidiumProjectileCoolDown);
-        largeProjectileWait = new WaitForSeconds(largeProjectileCoolDown);
-        afterLargeProjectileWait = new WaitForSeconds(afterLargePjojectileCoolDown);
-        beforeDashCoolDownWait = new WaitForSeconds(beforeDashCoolDown);
-        dashDurationWait = new WaitForSeconds(dashDuration);
-
-        spawnMissleMin = EnemySpawner.SPAWN_AREA_MIN + new Vector2(-2f, -1f);
-        spawnMissileMax = EnemySpawner.SPAWN_AREA_MAX + new Vector2(+2f, +1f);
     }
 
     public void Attack()
@@ -100,7 +78,7 @@ public class NecromancerAttack : ScriptableObject, IAttackPattern
                 yield break; // 적이 죽었거나 존재하지 않으면 코루틴 종료
             }
 
-            int randomNum = Random.Range(0, 2);
+            int randomNum = Random.Range(0, 3);
 
             switch (randomNum)
             {
@@ -109,17 +87,15 @@ public class NecromancerAttack : ScriptableObject, IAttackPattern
                     yield return enemy.StartCoroutine(CoSpawnMediumProjectiles());
                     break;
                 case 1:
-                    // 큰 크기의 발사체 오른쪽에서 발사하는 패턴 관련
-                    //yield return enemy.StartCoroutine(CoSpawnLargeProjectiles());
-
                     // 해골 미사일 발사체를 소환
+                    anim.SetTrigger("Skull"); // 해골 발사 애니메이션 시작
+                    yield return beforeSkullProjectileWait;
                     enemySpawner.SpawnEnemys(skullProjectileWave, skullProjectileCount);
                     yield return afterSkullProjectileWait;
                     break;
                 case 2:
-                case 3:
-                    // 대쉬
-                    yield return enemy.StartCoroutine(CoDash());
+                    // 폭발 패턴
+                    yield return enemy.StartCoroutine(CoExplosion());
                     break;
             }
             yield return attackWait;
@@ -164,69 +140,63 @@ public class NecromancerAttack : ScriptableObject, IAttackPattern
         yield return null; // 프레임 대기
     }
 
-    //IEnumerator CoSpawnLargeProjectiles()
-    //{
-    //    for (int i = 0; i < largeProjectileCount; i++)
-    //    {
-    //        yield return enemy.StartCoroutine(CoSpawnLargeProjectilesOnce());
-    //        yield return largeProjectileWait; // 다음 발사체 생성까지 대기
-    //    }
-    //    yield return afterLargeProjectileWait; // 큰 크기 발사체 생성 후 대기
-    //}
-
-    //IEnumerator CoSpawnLargeProjectilesOnce()
-    //{
-    //    // min_right_side_inde와 max_right_side_index 사이의 랜덤한 인덱스를 largeProjectileNumPerSpawn개수만큼 선택
-    //    int[] randomYPositions = new int[largeProjectileNumPerSpawn];
-    //    for (int i = 0; i < largeProjectileNumPerSpawn; i++)
-    //    {
-    //        int randomYPos;
-    //        do
-    //        {
-    //            randomYPos = Random.Range((int)spawnMissleMin.y, (int)spawnMissileMax.y + 1);
-    //        } while (System.Array.IndexOf(randomYPositions, randomYPos) != -1); // 중복 방지
-    //        randomYPositions[i] = randomYPos;
-    //    }
-
-    //    float Xpos = isOnRight ? spawnMissileMax.x : spawnMissleMin.x; // 오른쪽에서 생성할지 왼쪽에서 생성할지 결정 
-    //    // 큰 크기의 발사체들을 랜덤한 인덱스에서 생성
-    //    for (int i = 0; i < largeProjectileNumPerSpawn; i++)
-    //    {
-    //        Vector3 spawnPos = new Vector3(Xpos, randomYPositions[i], 0);
-    //        Vector3 randomPosition = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-1.5f, 1.5f), 0); // 약간의 랜덤 위치 조정
-    //        Quaternion rotate = isOnRight ? Quaternion.Euler(0, 0, 180) : Quaternion.Euler(0, 0, 0); // 오른쪽에서 생성할 때는 180도, 왼쪽에서 생성할 때는 0도 회전
-    //        Vector2 velocityDirection = isOnRight ? Vector2.left : Vector2.right; // 오른쪽에서 생성할 때는 왼쪽 방향, 왼쪽에서 생성할 때는 오른쪽 방향
-    //        GameObject proj = Instantiate(largeProjectilePrefab, spawnPos+ randomPosition, rotate);
-    //        proj.GetComponent<Rigidbody2D>().linearVelocity = velocityDirection * largeProjectileSpeed;
-
-    //    }
-    //    //SoundManager.Instance.PlaySFX("BlueDragonShootProjectile");
-    //    yield return null; // 프레임 대기
-
-    //}
-
-
-    IEnumerator CoDash()
+    IEnumerator CoExplosion()
     {
-        dashActivateCount++; // 대쉬 패턴을 실행한 횟수 증가
-        anim.SetBool("IsOnFast", true); // 대쉬 애니메이션 시작
-        yield return beforeDashCoolDownWait;
-        anim.SetBool("IsOnFast", false); // 대쉬 애니메이션 끝
+        List<List<EExplosionPosType>> explsionTypeList = new List<List<EExplosionPosType>>()
+        {
+            new List<EExplosionPosType>() { EExplosionPosType.Center, EExplosionPosType.Up, EExplosionPosType.Left },
+            new List<EExplosionPosType>() { EExplosionPosType.Center, EExplosionPosType.Up, EExplosionPosType.Right },
+            new List<EExplosionPosType>() { EExplosionPosType.Center, EExplosionPosType.Down, EExplosionPosType.Left },
+            new List<EExplosionPosType>() { EExplosionPosType.Center, EExplosionPosType.Down, EExplosionPosType.Right },
+            new List<EExplosionPosType>() { EExplosionPosType.LittleUp, EExplosionPosType.LittleDown, EExplosionPosType.Left},
+            new List<EExplosionPosType>() { EExplosionPosType.LittleUp, EExplosionPosType.LittleDown, EExplosionPosType.Right}
+        };
 
-        Vector2 directionToPlayer = (player.transform.position - enemy.transform.position).normalized;
-        enemy.rb.linearVelocity = directionToPlayer * dashSpeed;
-        yield return dashDurationWait;
+        for (int i = 0; i < explosionCount; i++)
+        {
+            int randomIndex = Random.Range(0, explsionTypeList.Count);
+            List<EExplosionPosType> explosionPosTypes = explsionTypeList[randomIndex];
+            for (int j = 0; j < explosionPosTypes.Count; j++)
+            {
+                Vector3 explosionPosition = player.transform.position; // 플레이어의 현재 위치를 기준으로 폭발 위치 설정
+                switch (explosionPosTypes[j])
+                {
+                    case EExplosionPosType.Center:
+                        break;
+                    case EExplosionPosType.Up:
+                        explosionPosition += Vector3.up * 4f; 
+                        break;
+                    case EExplosionPosType.Down:
+                        explosionPosition += Vector3.down * 4f; 
+                        break;
+                    case EExplosionPosType.Left:
+                        explosionPosition += Vector3.left * 4f;
+                        break;
+                    case EExplosionPosType.Right:
+                        explosionPosition += Vector3.right * 4f;
+                        break;
+                    case EExplosionPosType.LittleUp:
+                        explosionPosition += Vector3.up * 2f; 
+                        break;
+                    case EExplosionPosType.LittleDown:
+                        explosionPosition += Vector3.down * 2f; 
+                        break;
+                }
+                 Instantiate(explosionPrefab, explosionPosition, Quaternion.identity);
+            }
 
-        enemy.rb.linearVelocity = Vector2.zero; // 대쉬가 끝나면 속도를 0으로 설정
-        isOnRight = !isOnRight; // 대쉬 후 방향을 반대로 변경
-        spriteRenderer.transform.rotation = Quaternion.Euler(0, isOnRight ? 0 : 180, 0); // 스프라이트 방향 변경
-
-        Vector3 newPos = isOnRight ? new Vector3(spawnMissileMax.x + 4f, 0, 0) : new Vector3(spawnMissleMin.x - 4f, 0, 0);
-        enemy.transform.parent.position = newPos; // 대쉬 후 위치를 오른쪽 혹은 왼쪽으로 이동
-        Vector2 direction = isOnRight ? Vector2.left : Vector2.right;
-        enemy.rb.linearVelocity = direction * enemy.speed * 2;
-        yield return new WaitForSeconds(1.5f);
-        enemy.rb.linearVelocity = Vector2.zero;
-
+            yield return explosionWait; // 폭발 사이 간격 대기
+        }
     }
+}
+
+public enum EExplosionPosType // 플레이어를 중심으로 한 상대적 위치
+{
+    Center,
+    Up,
+    Down,
+    Left,
+    Right,
+    LittleUp,
+    LittleDown,
 }
