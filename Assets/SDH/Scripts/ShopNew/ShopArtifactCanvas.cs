a1d40a7325e90b61f6587f416937ef465b8e02e5
pdefd77 <pdefd77@gmail.com>
2025-06-21 18:56:21 +0900

    [fix] 유물 풀 제거 시 무한루프 버그 수정

--- Assets/SDH/Scripts/ShopNew/ShopArtifactCanvas.cs
+++ Assets/SDH/Scripts/ShopNew/ShopArtifactCanvas.cs
@@ -9,20 +9,7 @@ public class ShopArtifactCanvas : MonoBehaviour
 
     private void Start()
     {
-        if (Managers.Artifact.IsFullArtifact) Destroy(gameObject);
-
-        do
-        {
-            artifactIdx = Random.Range(0, Managers.Artifact.ArtifactLists.Count);
-        }
-        while (Managers.Artifact.ArtifactLists[artifactIdx].isPurchased); // 구매하지 않은 유물로 채우기
-
-        artifactTxt.text = Managers.Artifact.ArtifactLists[artifactIdx].explain;
-    }
-
-    private void OnEnable()
-    {
-        if (Managers.Artifact.IsFullArtifact) Destroy(gameObject);
+        if (Managers.Artifact.IsArtifactMax()) Destroy(gameObject);
 
         do
         {
